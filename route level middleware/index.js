13277b52fbcfd329c5ff7caa48fbe038d9abc93c
Nikhil Mandora <nikhilmandoranm@gmail.com>
2024-06-05 08:53:01 +0530

    Database

--- route level middleware/index.js
+++ route level middleware/index.js
@@ -18,6 +18,7 @@ const m2 = (req,res,next)=>{
 
 const m3 = (req,res,next)=>{
     console.log("M3 Called");
+    next();
 };
 
 router1.use(m1);
@@ -31,19 +32,19 @@ app.get("/" , (req,res)=>{
 });
 
 app.post("/" , (req,res)=>{
-    res.send("Hello with app (Get)");
+    res.send("Hello with app (Post)");
 });
 
 app.put("/" , (req,res)=>{
-    res.send("Hello with app (Get)");
+    res.send("Hello with app (Put)");
 });
 
 app.patch("/" , (req,res)=>{
-    res.send("Hello with app (Get)");
+    res.send("Hello with app (Patch)");
 });
 
 app.delete("/" , (req,res)=>{
-    res.send("Hello with app (Get)");
+    res.send("Hello with app (Delete)");
 });
 
 
@@ -54,19 +55,19 @@ router1.get("/" , (req,res)=>{
 });
 
 router1.post("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 1 (Post)");
 });
 
 router1.put("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 1 (Put)");
 });
 
 router1.patch("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 1 (Patch)");
 });
 
 router1.delete("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 1 (Delete)");
 });
 
 // Router 2
@@ -80,37 +81,37 @@ router2.post("/" , (req,res)=>{
 });
 
 router2.put("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 2 (Put)");
 });
 
 router2.patch("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 2 (Patch)");
 });
 
 router2.delete("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 2 (Delete)");
 });
 
 // Router 3
 
 router3.get("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 3 (Get)");
 });
 
 router3.post("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 3 (Post)");
 });
 
 router3.put("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 3 (Put)");
 });
 
 router3.patch("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 3 (Patch)");
 });
 
 router3.delete("/" , (req,res)=>{
-    res.send("Hello with Router 1 (Get)");
+    res.send("Hello with Router 3 (Delete)");
 });
 
 
