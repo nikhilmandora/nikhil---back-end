9c292cfcfd4983c783bd359659be2a774bf94bbe
Nikhil Mandora <nikhilmandoranm@gmail.com>
2024-06-05 15:31:47 +0530

    read data

--- 008 readdata/index.js
+++ 008 readdata/index.js
@@ -1,1 +1,31 @@
 const express = require("express");
+const app = express();
+const {MongoClient} = require("mongodb");
+
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+client.connect();
+
+app.use(express.json());
+
+app.get("/" , (req,res)=>{
+    try{
+        res.status(200).json({msg : "Welcome to api"});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({msg : "Internal Server Error"});
+    }
+});
+
+app.post("/adddtata" , (req , res)=>{
+    try{
+        console.log(req.body);
+        res.status(200).json({msg : "okay"});
+    } catch (error) {
+        res.status(500).json({msg : "Internal Server Error"})
+    }
+});
+
+app.listen(4000 , ()=>{
+    console.log("Server is running on Port 4000");
+});
