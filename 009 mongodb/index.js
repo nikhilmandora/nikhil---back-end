135a2d7e82f43cf645c90abcdbbd486759cfd0d6
Nikhil Mandora <nikhilmandoranm@gmail.com>
2024-06-07 13:42:05 +0530

    MongoDB

--- 009 mongodb/index.js
+++ 009 mongodb/index.js
@@ -15,10 +15,10 @@ const database = async()=>{
 const insertData = async()=>{
     const db = await database();
 
-    const adminCollection = await db.collection("users");
+    const adminCollection = await db.collection("admins");
 
     const response = await adminCollection.insertOne({
-        name:"userName1",
+        name:"adminName1",
         password:"user@123"
     });
     console.log(response);
@@ -41,9 +41,39 @@ const readData = async()=>{
 
 // readData();
 
+const deleteData = async()=>{
+    try{
+        const db = await database();
+        const collection = await db.collection("admins");
 
+        const response = await collection.deleteOne({
+            name: 'userId1'
+        });
+        console.log(response);
 
+    } catch(error) {
+        console.log(error);
+    }
+};
 
+// deleteData();
+
+const updateData = async()=>{
+    try{
+        const db = await database();
+        const collection = await db.collection("admins");
+
+        const response = await collection.updateOne({name: 'userId1'} ,
+            {$set:{name:"userId007"}}
+        );
+        console.log(response);
+
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+// updateData();
 
 app.use(express.json());
 
